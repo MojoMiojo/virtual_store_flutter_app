ff9ac2a6bcf8f492f3bed64daf108972574604da
Mozart Sousa <mozartsousa@frwk.com.br>
2020-10-01 17:26:47 -0300

    home_screen fix, drawer created.
    Co-Authored-By: Mozart Sousa <mjasousa@sga.pucminas.br>

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:virtual_store_flutter_app/tabs/home_tab.dart';
+import 'package:virtual_store_flutter_app/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   final _pageController = PageController();
@@ -10,7 +11,10 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController,
       //physics: NeverScrollableScrollPhysics(),
       children: [
-        HomeTab()
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(),
+        )
       ],
     );
   }
