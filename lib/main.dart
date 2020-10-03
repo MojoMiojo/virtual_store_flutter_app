06eb49be8bdf4e4238ab93c32844fda759f89286
Mozart Sousa <mozartsousa@frwk.com.br>
2020-10-03 14:25:26 -0300

    ScopedModel created
    Co-Authored-By: Mozart Sousa <mjasousa@sga.pucminas.br>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:virtual_store_flutter_app/models/user_model.dart';
 import 'package:virtual_store_flutter_app/screens/home_screen.dart';
 import 'package:virtual_store_flutter_app/screens/login_screen.dart';
 import 'package:virtual_store_flutter_app/screens/signup_screen.dart';
@@ -16,16 +18,19 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter's Clothing",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141)
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: MaterialApp(
+        title: "Flutter's Clothing",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Color.fromARGB(255, 4, 125, 141)
+        ),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+        //home: LoginScreen()
+        //home: SignUpScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      //home: LoginScreen()
-      //home: SignUpScreen(),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:virtual_store_flutter_app/models/user_model.dart';
 import 'package:virtual_store_flutter_app/screens/signup_screen.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -28,69 +30,77 @@ class LoginScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "E-mail",
-              ),
-              keyboardType: TextInputType.emailAddress,
-              // ignore: missing_return
-              validator: (text){
-                if(text.isEmpty || !text.contains("@")) return "E-mail inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Senha",
-              ),
-              obscureText: true,
-              validator: (text){
-                // ignore: missing_return
-                if(text.isEmpty || text.length < 6) return "Senha inválida!";
-              },
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                padding: EdgeInsets.all(0),
-                onPressed: () {},
-                child: Text(
-                  "Esqueci minha senha",
-                  textAlign: TextAlign.right,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              height: 44,
-              child: RaisedButton(
-                onPressed: (){
-                  if(_formKey.currentState.validate()){
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model){
+          if(model.isLoading)
+            return Center(child: CircularProgressIndicator(),);
 
-                  }
-                },
-                child: Text(
-                  "Entrar",
-                  style: TextStyle(
-                      fontSize: 18
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "E-mail",
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  // ignore: missing_return
+                  validator: (text){
+                    if(text.isEmpty || !text.contains("@")) return "E-mail inválido!";
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Senha",
+                  ),
+                  obscureText: true,
+                  // ignore: missing_return
+                  validator: (text){
+                    if(text.isEmpty || text.length < 6) return "Senha inválida!";
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {},
+                    child: Text(
+                      "Esqueci minha senha",
+                      textAlign: TextAlign.right,
+                    ),
                   ),
                 ),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-              ),
-            )
-          ],
-        ),
-      ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+
+                      }
+                      model.signIn();
+                    },
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                          fontSize: 18
+                      ),
+                    ),
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      )
     );
   }
 }
