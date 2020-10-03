9d8db9b04bc0286650974666d874764051292e1d
Mozart Sousa <mozartsousa@frwk.com.br>
2020-10-03 13:12:07 -0300

    Gridview. Size of the product created.
    Co-Authored-By: Mozart Sousa <mjasousa@sga.pucminas.br>

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_store_flutter_app/datas/product_data.dart';
 
@@ -14,7 +15,7 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
   _ProductScreenState(this.product);
-
+  String size;
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +67,49 @@ class _ProductScreenState extends State<ProductScreen> {
                     fontWeight: FontWeight.w500,
                     color: primaryColor,
                   ),
-                )
+                ),
+                SizedBox(height: 16,),
+                Text(
+                  "Tamanho",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 34,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      //linhas
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.5,
+                    ),
+                    children: product.sizes.map((s) {
+                      return GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            size = s;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            border: Border.all(
+                              color: s == size ? primaryColor : Colors.grey[500],
+                              width: 3,
+                            )
+                          ),
+                          width: 50,
+                          alignment: Alignment.center,
+                          child: Text(s),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
               ],
             ),
           )
