d1f93a1c21c1710c5d51145468b19bab3e72af5e
Mozart Sousa <mozartsousa@frwk.com.br>
2020-10-03 16:20:55 -0300

    Create user in firebase added
    Co-Authored-By: Mozart Sousa <mjasousa@sga.pucminas.br>

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,22 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model{
   //usuário atual
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  FirebaseUser firebaseUser;
+  Map<String,dynamic> userData = Map();
 
   bool isLoading = false;
 
-  void signUp(){
+  void begin(){
+    this.isLoading = true;
+    notifyListeners();
+  }
 
+  void end(){
+    this.isLoading = false;
+    notifyListeners();
+  }
+
+  void signUp({
+    @required Map<String, dynamic> userData,
+    @required String pass,
+    @required VoidCallback onSuccess,
+    @required VoidCallback onFail})
+  {
+
+    begin();
+
+    _auth.createUserWithEmailAndPassword(
+        email: userData["email"],
+        password: pass
+    ).then((user)async{
+      firebaseUser = user;
+
+      await _saveUserData(userData);
+      
+      onSuccess();
+      end();
+    }).catchError((e){
+      onFail();
+      end();
+    });
   }
 
   void  signIn()async{
-    isLoading = true;
-    notifyListeners();
-
+    begin();
     await Future.delayed(Duration(seconds: 3));
-
-    isLoading = false;
-    notifyListeners();
+    end();
   }
 
   void recoverPass(){
@@ -26,4 +60,13 @@ class UserModel extends Model{
   bool isLoggedIn(){
     return false;
   }
+
+  Future<Null> _saveUserData(Map<String,dynamic> userData)async{
+    this.userData = userData;
+    await Firestore.instance
+        .collection("users")
+        .document(firebaseUser.uid)
+        .setData(userData);
+  }
+
 }
