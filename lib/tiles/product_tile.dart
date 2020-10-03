3c86e18a11726e9418fe796348687dbfeb732813
Mozart Sousa <mozartsousa@frwk.com.br>
2020-10-03 12:14:58 -0300

    List tile created
    Co-Authored-By: Mozart Sousa <mjasousa@sga.pucminas.br>

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_store_flutter_app/datas/product_data.dart';
+import 'package:virtual_store_flutter_app/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
@@ -8,11 +9,14 @@ class ProductTile extends StatelessWidget {
 
   ProductTile(this.type, this.product);
 
-
-
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>ProductScreen(product))
+        );
+      },
       child: Card(
         child: type == "grid"
             ? Column(
@@ -49,7 +53,41 @@ class ProductTile extends StatelessWidget {
                   )
                 ],
               )
-            : Row(),
+            : Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                      height: 250,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child:  Container(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$ ${product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
       ),
     );
   } //end build
