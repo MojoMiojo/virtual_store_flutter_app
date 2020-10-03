1a82d9ba2f3b5aa866fd21afad407e5068519925
Mozart Sousa <mozartsousa@frwk.com.br>
2020-10-03 12:31:15 -0300

    product screen carousel created
    Co-Authored-By: Mozart Sousa <mjasousa@sga.pucminas.br>

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_store_flutter_app/datas/product_data.dart';
 
@@ -14,8 +15,63 @@ class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
   _ProductScreenState(this.product);
 
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 0.9,
+            //Carrosel de imagens
+            child: Carousel(
+              images: product.images.map((url){
+                return NetworkImage(url);
+              }).toList(),
+              //tamanho do ponto
+              dotSize: 4.0,
+              //espaçamento do ponto
+              dotSpacing: 15,
+              //cor de fundo do ponto
+              dotBgColor: Colors.transparent,
+              //cor do ponto
+              dotColor: primaryColor,
+              //rodar automaticamente
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                    color: primaryColor,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
